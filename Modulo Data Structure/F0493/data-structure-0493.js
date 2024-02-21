10ba0836d5818aa79ed651b5bf24d15bf27f3003
Rafael <contato.rafaelgomes@outlook.com>
2024-02-20 22:00:05 -0300

    array interator for i, for of and for in

--- Modulo Data Structure/F0493/data-structure-0493.js
+++ Modulo Data Structure/F0493/data-structure-0493.js
@@ -7,3 +7,19 @@ console.log(pilotos[3]);
 // acessando o comprimento deste array
 console.log("\nacessando o comprimento do array");
 console.log(pilotos.length);
+
+// interando o array
+console.log("\ninterando o array com for i");
+for (let i = 0; i < pilotos.length; i++) {
+  console.log(pilotos[i]);
+}
+
+console.log("\ninterando o array com for of");
+for (let piloto of pilotos) {
+  console.log(piloto);
+}
+
+console.log("\ninterando o array com for in");
+for (let piloto in pilotos) {
+  console.log(pilotos[piloto]);
+}
