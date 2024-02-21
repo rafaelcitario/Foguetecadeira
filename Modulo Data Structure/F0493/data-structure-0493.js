021665744c56cb3c678d72b7953cdfb2e0799984
Rafael <contato.rafaelgomes@outlook.com>
2024-02-20 21:58:55 -0300

    indexation

---
+++ Modulo Data Structure/F0493/data-structure-0493.js
@@ -0,0 +1,7 @@
+const pilotos = ["Sena", "Prost", "Schumacher", "Hamilton"];
+// indexação começa pelo zero.
+console.log("\nindexação começa a partir do zero.");
+console.log(pilotos[0]);
+console.log(pilotos[3]);
+
+
