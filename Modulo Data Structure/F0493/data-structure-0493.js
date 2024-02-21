673550ae51398de1fad9cced635bddf2e4b571eb
Rafael <contato.rafaelgomes@outlook.com>
2024-02-20 21:59:23 -0300

    array lenght

--- Modulo Data Structure/F0493/data-structure-0493.js
+++ Modulo Data Structure/F0493/data-structure-0493.js
@@ -4,4 +4,6 @@ console.log("\nindexação começa a partir do zero.");
 console.log(pilotos[0]);
 console.log(pilotos[3]);
 
-
+// acessando o comprimento deste array
+console.log("\nacessando o comprimento do array");
+console.log(pilotos.length);
