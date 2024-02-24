bfb63df9e6797b4069dd88d0b352a13975b56243
Rafael <contato.rafaelgomes@outlook.com>
2024-02-24 17:30:34 -0300

    create server return json infos about one github user

--- Module Js 3 Assync/F0637/src/server/index.js
+++ Module Js 3 Assync/F0637/src/server/index.js
@@ -1,6 +1,29 @@
+const express = require("express");
+const app = express();
 const axios = require("axios");
 const GET_API = {
-  url: "https://api.github.com/users/rafaelcitario",
+  requestUrl: "https://api.github.com/users/rafaelcitario",
+  host: "localhost",
+  port: 3000,
 };
-axios.get(GET_API.url)
-.then(response => console.log(response))
+
+app.get("/", (req, res, next) => {
+  axios
+    .get(GET_API.requestUrl)
+    .then((response) => response)
+    .then(({ data }) =>
+      res.json({
+        login: data.login,
+        avatar: data.avatar_url,
+        href: data.html_url,
+        repos: data.repos_url,
+        publicRepos: data.public_repos,
+        followers: data.followers,
+      })
+    )
+    .catch((err) => console.log(err));
+});
+
+app.listen(GET_API.port, () =>
+  console.log(`SERVER IS RUNNING AT ${GET_API.host}:${GET_API.port}`)
+);
