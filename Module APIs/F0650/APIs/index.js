9d165ee1177db585cec7249ad1f4b1c6826b6c1c
Rafael <contato.rafaelgomes@outlook.com>
2024-02-24 19:56:16 -0300

    method get and post is done! POST has not return yet.

--- Module APIs/F0650/APIs/index.js
+++ Module APIs/F0650/APIs/index.js
@@ -5,6 +5,8 @@ const SERVER = {
   HOST: "localhost",
 };
 
+app.get("/", (request, response) => response.send("Esta é a página HOME"));
+app.post("/contato", (req, res) => console.log(req.body));
 app.listen(SERVER.PORT, () =>
   console.log(`Acess: ${SERVER.HOST}:${SERVER.PORT}`)
 );
