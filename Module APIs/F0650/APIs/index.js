1df86a9bf41d279b31b37755bd5f4d7bf5f985eb
Rafael <contato.rafaelgomes@outlook.com>
2024-02-24 20:08:03 -0300

    add middleware app.use(express.json()). Now request post is done!

--- Module APIs/F0650/APIs/index.js
+++ Module APIs/F0650/APIs/index.js
@@ -4,9 +4,9 @@ const SERVER = {
   PORT: 3000,
   HOST: "localhost",
 };
-
+app.use(express.json());
 app.get("/", (request, response) => response.send("Esta é a página HOME"));
-app.post("/contato", (req, res) => console.log(req.body));
+app.post("/contato", (req, res) => res.status(200).send(req.body.content));
 app.listen(SERVER.PORT, () =>
   console.log(`Acess: ${SERVER.HOST}:${SERVER.PORT}`)
 );
