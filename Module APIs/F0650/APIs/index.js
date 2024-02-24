e240a84dccfa7e7182ce7ee0e0f2793ac26547cd
Rafael <contato.rafaelgomes@outlook.com>
2024-02-24 20:20:03 -0300

    use method put to update obj user

--- Module APIs/F0650/APIs/index.js
+++ Module APIs/F0650/APIs/index.js
@@ -6,7 +6,16 @@ const SERVER = {
 };
 app.use(express.json());
 app.get("/", (request, response) => response.send("Esta é a página HOME"));
-app.post("/contato", (req, res) => res.status(200).send(req.body.content));
+app.post("/contato", (req, res) => res.status(200).send(req.body.content)); // @ insomnia: {"content":"rafael"}
+
+app.put("/put", (req, res) => {
+  let user = { name: "Rafael", age: 28, nationality: "Brazilian" };
+  console.log(user); // { name: "Rafael", age: 28, nationality: "Brazilian" }
+  user = req.body; // @ insomnia: { name: 'Rafael Gomes', age: 28, nationality: 'Brazilian' }
+  res.send(user);
+  console.log(user); // { name: 'Rafael Gomes', age: 28, nationality: 'Brazilian' }
+});
+
 app.listen(SERVER.PORT, () =>
   console.log(`Acess: ${SERVER.HOST}:${SERVER.PORT}`)
 );
