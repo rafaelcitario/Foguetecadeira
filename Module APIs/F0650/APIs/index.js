28a1d8f87c2bba375bfda871c3f7af8c161297bd
Rafael <contato.rafaelgomes@outlook.com>
2024-02-24 21:15:40 -0300

    method delete

--- Module APIs/F0650/APIs/index.js
+++ Module APIs/F0650/APIs/index.js
@@ -11,10 +11,17 @@ app.post("/contato", (req, res) => res.status(200).send(req.body.content)); // @
 app.put("/put", (req, res) => {
   let user = { name: "Rafael", age: 28, nationality: "Brazilian" };
   console.log(user); // { name: "Rafael", age: 28, nationality: "Brazilian" }
-  user = req.body; // @ insomnia: { name: 'Rafael Gomes', age: 28, nationality: 'Brazilian' }
+  user = req.body; // @ insomnia: { name: 'Rafael Citario', age: 28, nationality: 'Brazilian' }
   res.send(user);
-  console.log(user); // { name: 'Rafael Gomes', age: 28, nationality: 'Brazilian' }
+  console.log(user); // { name: 'Rafael Citario', age: 28, nationality: 'Brazilian' }
 });
+
+app.delete("/delete/:id", (req, res) => {
+  res.send(req.params.id);
+});
+
+
+
 
 app.listen(SERVER.PORT, () =>
   console.log(`Acess: ${SERVER.HOST}:${SERVER.PORT}`)
